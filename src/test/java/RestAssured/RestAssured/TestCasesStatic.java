370a0672f78d8f14f4ab55ba1dc93b3503d589c8
Priyanka Sharma <itspriyanka.ps11@gmail.com>
2021-02-15 12:08:29 +0530

    added del,put,patch,post API methods and created config file

--- src/test/java/RestAssured/RestAssured/TestCasesStatic.java
+++ src/test/java/RestAssured/RestAssured/TestCasesStatic.java
@@ -1,6 +1,9 @@
 package RestAssured.RestAssured;
 
+import org.json.simple.JSONObject;
 import org.junit.Assert;
+
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 
@@ -8,14 +11,27 @@ import io.restassured.response.Response;
 
 import static io.restassured.RestAssured.*;
 
+import java.io.FileInputStream;
+
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Properties;
 
 public class TestCasesStatic {
+	public static String BaseURIVar;
+	
+	@BeforeClass
+	public static void setup() throws IOException {
+		FileInputStream ConfigFile=new FileInputStream("./Config.properties");
+		Properties configProp= new Properties();
+		configProp.load(ConfigFile);
+		BaseURIVar=configProp.getProperty("BaseURI");
+		
+	}
 	
 	@Test
 	public void getAPI() {
-		baseURI= "https://reqres.in";
+		baseURI= BaseURIVar;
 		
 		Response resp=given().when().get("/api/users?page=2").then().extract().response();
 		
@@ -35,5 +51,72 @@ public class TestCasesStatic {
 		String header1=resp.header("Content-Type");
 		Assert.assertEquals(header1 ,"application/json; charset=utf-8");
 	}
+	
+	@Test
+	public void postAPI() {
+		baseURI=BaseURIVar;
+		
+		JSONObject req= new JSONObject();
+		req.put("name", "Oracle");
+		req.put("job", "Foreseeing");
+		
+		Response resp1=given().body(req.toJSONString()).when().post("/api/users").then().extract().response();
+		
+		int statusCode= resp1.getStatusCode();
+		Assert.assertEquals(statusCode, 201);
+		
+		String id=resp1.getBody().path("id");
+		System.out.println(id);
+	}
+	
+	@Test
+	public void putAPI() {
+		baseURI=BaseURIVar;
+		JSONObject reqput= new JSONObject();
+		reqput.put("name", "Oracle");
+		reqput.put("job", "Foreseeing");
+		
+		Response respput=given().body(reqput.toJSONString()).when().put("/api/users/2").then().extract().response();
+		
+		int StatusCode=respput.getStatusCode();
+		Assert.assertEquals(StatusCode, 200);
+		
+		String name=respput.getBody().path("name");
+		Assert.assertEquals(name, "Oracle");
+		
+		String job=respput.getBody().path("job");
+		Assert.assertEquals(job, "Foreseeing");
+		
+		
+	}
+	
+	@Test 
+	public void patchAPI() {
+		baseURI=BaseURIVar;
+		JSONObject reqput= new JSONObject();
+		reqput.put("name", "Oracle");
+		reqput.put("job", "Foreseeing");
+		
+		Response respput=given().body(reqput.toJSONString()).when().patch("/api/users/2").then().extract().response();
+		
+		int StatusCode=respput.getStatusCode();
+		Assert.assertEquals(StatusCode, 200);
+	}
 
+	@Test
+	public void deleteAPI() {
+		baseURI=BaseURIVar;
+		
+		given().when().delete("/api/users/2").then().assertThat().statusCode(204);
+	}
+	
+	@Test
+	public void authorizationAPI() {
+		baseURI=BaseURIVar;
+		Response respa=given().auth().basic("eve.holt@reqres.in", "cityslicka").when().get("/api/login").then().extract().response();
+		
+		Assert.assertEquals(respa.getStatusCode(), 200);
+		
+		System.out.println(respa.getBody().path("token"));
+	}
 }
